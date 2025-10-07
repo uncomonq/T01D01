11b9e91d904aa5b7c414993bf492d371ca928914
uncomonq <uncomonq@mail.ru>
2025-10-07 20:18:02 +0300

    fix Quest3

--- src/quest3.sh
+++ src/quest3.sh
@@ -1,23 +1,19 @@
-touch src/quest3.sh
-# Создание необходимых директорий
-mkdir -p door_management_files/door_configuration
-mkdir -p door_management_files/door_map
-mkdir -p door_management_files/door_logs
-# Добавление тестовых конфигурационных файлов
-echo "config=door_1" > door_management_files/door_configuration/main.conf
-# Добавление карты дверей
-echo "Door map version 1.1" > door_management_files/door_map/door_map_1.1
-# Добавление логов
-echo "Door opened successfully" > door_management_files/door_logs/door_1.log
+#!/bin/bash
 
-# Создание дополнительных .conf файлов (30 штук)
-for i in {2..30}
-do
-  echo "config=door_$i" > door_management_files/door_configuration/door_$i.conf
-done
+OLD_DIR="door_management_fi"
+NEW_DIR="door_management_files"
 
-# Создание дополнительных .log файлов (2 штуки, вместе с door_1.log будет 3)
-for i in {2..3}
-do
-  echo "Door log $i" > door_management_files/door_logs/door_$i.log
-done
+if [ ! -d "$OLD_DIR" ]; then
+    exit 1
+fi
+
+mv "$OLD_DIR" "$NEW_DIR"
+
+cd "$NEW_DIR" || exit
+
+mkdir -p door_configuration door_logs door_map
+
+mv *.conf 2>/dev/null door_configuration/
+mv *.log 2>/dev/null door_logs/
+mv *.1 2>/dev/null door_map/
+
