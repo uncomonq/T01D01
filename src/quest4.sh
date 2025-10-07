46bbe8b9fe91d9dee49634758ddc7b04eee40183
uncomonq <uncomonq@mail.ru>
2025-10-07 20:28:17 +0300

    fix Quest4

--- src/quest4.sh
+++ src/quest4.sh
@@ -1,3 +1,14 @@
-ps aux | grep ai_door_control
-kill 2681
+#!/bin/bash
 
+PIDS=$(ps aux | grep '[a]i_door_control.sh' | awk '{print $2}')
+
+if [ "$PIDS" ]; then
+
+    for PID in $PIDS; do
+        kill "$PID" 2>/dev/null
+        sleep 1
+        if ps -p "$PID" > /dev/null 2>&1; then
+            kill -9 "$PID"
+        fi
+    done
+fi
